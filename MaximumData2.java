d782f31a74df9a757cae7a3451476131eeebb6d4
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-07-20 12:03:01 +0530

    find the maximum String from given String

---
+++ MaximumData2.java
@@ -0,0 +1,28 @@
+package com.BridgeLabz.generic;
+
+public class MaximumData2 {
+	 public void maxElement(Integer[] inputArray) {  //1,2,3
+	        for(int i=0; i<inputArray.length-1; i++) {
+	            if(inputArray[i].compareTo(inputArray[i+1])==1) {
+	                inputArray[i+1] = inputArray[i];
+	            }
+	        }
+	        System.out.println(inputArray[inputArray.length-1]);
+	    }
+	    public void maxElement(Float[] inputArray) {
+	        for(int i=0; i<inputArray.length-1; i++) {
+	            if(inputArray[i].compareTo(inputArray[i+1])==1) {
+	                inputArray[i+1] = inputArray[i];
+	            }
+	        }
+	        System.out.println(inputArray[inputArray.length-1]);
+	    }
+	    public void maxElement(String[] inputArray) {
+	        for(int i=0; i<inputArray.length-1; i++) {
+	            if(inputArray[i].compareTo(inputArray[i+1]) > 0) {
+	                inputArray[i+1] = inputArray[i];
+	            }
+	        }
+	        System.out.println(inputArray[inputArray.length - 1]);
+	    }
+}
